a3db26a645ce872ee18a2438523966a49569732f
Alexander Kay <a.kravchuk@avanade.com>
2022-11-16 14:16:17 -0800

    local env changed

--- src/lib/dictionary-service-factory.ts
+++ src/lib/dictionary-service-factory.ts
@@ -12,6 +12,7 @@ export class DictionaryServiceFactory {
           endpoint: config.graphQLEndpoint,
           apiKey: config.sitecoreApiKey,
           siteName: config.jssAppName,
+          rootItemId: '{52D101EC-0F13-5608-844B-604BE0ABC2ED}',
           /*
             The Dictionary Service needs a root item ID in order to fetch dictionary phrases for the current
             app. If your Sitecore instance only has 1 JSS App, you can specify the root item ID here;
